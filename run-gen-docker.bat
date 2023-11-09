a474e3f3616dbce075e94ebf8cd5f1d6401636a5
Dmitry Egorov <egorovdmi@gmail.com>
2023-11-09 16:48:48 +0100

    Fixed errors in bat files

--- run-gen-docker.bat
+++ run-gen-docker.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set FILE_SIZE_LIMIT=10GB
-set OUTPUT_FILE_PATH="../Data/testfile.txt"
+set OUTPUT_FILE_PATH=../Data/testfile.txt
 
 docker build -t my-large-file-generator -f ./App.Cmd.Generator/Dockerfile .
 docker run -v %cd%/Data:/app/Data --env FILE_SIZE_LIMIT=%FILE_SIZE_LIMIT% --env OUTPUT_FILE_PATH=%OUTPUT_FILE_PATH% -it --rm my-large-file-generator
--- run-gen.bat
+++ run-gen.bat
@@ -1,6 +1,6 @@
 @echo off
 
 set FILE_SIZE_LIMIT=10GB
-set OUTPUT_FILE_PATH="../Data/testfile.txt"
+set OUTPUT_FILE_PATH=../Data/testfile.txt
 
 cd App.Cmd.Generator && dotnet restore && dotnet run
