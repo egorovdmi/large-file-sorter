0df1b8f0173f807602120b3feaede51162e78474
Dmitry Egorov <egorovdmi@gmail.com>
2023-11-09 13:19:38 +0100

    Added environment variables to bat scripts

--- run-sort.bat
+++ run-sort.bat
@@ -1,2 +1,8 @@
 @echo off
+
+set INPUT_FILE_PATH="../Data/testfile.txt"
+set OUTPUT_FILE_PATH="../Data/testfile_sorted.txt"
+set MEMORY_LIMIT="16GB"
+set TEMP_DIR="../Data/Temp"
+
 cd App.Cmd.Sorter && dotnet restore && dotnet run
