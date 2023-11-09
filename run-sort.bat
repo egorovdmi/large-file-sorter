2232ab2a844ad23ec5a9f3d69b099c20efb3f31a
Dmitry Egorov <egorovdmi@gmail.com>
2023-11-09 17:43:04 +0100

    Increased memory limit

--- run-sort.bat
+++ run-sort.bat
@@ -2,7 +2,7 @@
 
 set INPUT_FILE_PATH=../Data/testfile.txt
 set OUTPUT_FILE_PATH=../Data/testfile_sorted.txt
-set MEMORY_LIMIT=16GB
+set MEMORY_LIMIT=32GB
 set TEMP_DIR=../Data/Temp
 
 cd App.Cmd.Sorter && dotnet restore && dotnet run
